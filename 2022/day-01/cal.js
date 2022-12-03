e9c48b3e1344c5284ec44e09cbf5630b3c395815
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2022-12-03 12:23:15 -0600

    Comments added

--- 2022/day-01/cal.js
+++ 2022/day-01/cal.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
-
 const getCalories = () => {
+
+    // Reading File input.txt
     fs.readFile('input.txt', 'utf8', (err, data) => {
-      if (err) {
+        if (err) {
             console.error(err);
             return;
         }
 
+        // Sort calories into arrays.
         const caloriesSummedUp = 
             data 
             .split('\n\n')
@@ -15,9 +16,12 @@ const getCalories = () => {
             .map(c => Number(c))
             .sort((a,b) => a < b ? 1 : a > 0 ? -1 : 0)
 
+        // Returns Most Calories
         const getMostCalories = caloriesSummedUp.slice(0, 1)
+        // Sum of top three calorie groups
         const getSumOfTopThree = caloriesSummedUp.slice(0, 3).reduce((a,b) => a + b)
 
+        // Will display the greatest amount of calories and the sum of the top three most amount of calories.
         console.log('The Most Calories:', getMostCalories)
         console.log('This is the total of the top three:', getSumOfTopThree)
     });
