78a71048d60ea811980fe7704dd24b661bcd535c
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2022-12-04 18:04:07 -0600

    Comments added to day one files and Day two challenge completion

--- 2022/day-01/cal.js
+++ 2022/day-01/cal.js
@@ -7,23 +7,23 @@ const getCalories = () => {
             return;
         }
 
-        // Sort calories into arrays.
+        // Sort all calories into arrays.
         const caloriesSummedUp = 
             data 
             .split('\n\n')
             .map(x => x.split('\n')
             .reduce((a,b) => Number(a) + Number(b)))
             .map(c => Number(c))
-            .sort((a,b) => a < b ? 1 : a > 0 ? -1 : 0)
+            .sort((a,b) => a < b ? 1 : a > 0 ? -1 : 0);
 
-        // Returns Most Calories
-        const getMostCalories = caloriesSummedUp.slice(0, 1)
-        // Sum of top three calorie groups
-        const getSumOfTopThree = caloriesSummedUp.slice(0, 3).reduce((a,b) => a + b)
+        // Returns Most Calories - FOR PART 1
+        const getMostCalories = caloriesSummedUp.slice(0, 1);
+        // Sum of top three calorie groups - FOR PART 2
+        const getSumOfTopThree = caloriesSummedUp.slice(0, 3).reduce((a,b) => a + b);
 
         // Will display the greatest amount of calories and the sum of the top three most amount of calories.
-        console.log('The Most Calories:', getMostCalories)
-        console.log('This is the total of the top three:', getSumOfTopThree)
+        console.log('The Most Calories:', getMostCalories);
+        console.log('This is the total of the top three:', getSumOfTopThree);
     });
 }
 
