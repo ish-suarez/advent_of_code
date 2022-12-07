c02719a5369d04cb3ec088c9cf8a0a60e48fb2a0
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2022-12-06 18:01:05 -0600

    Part 2 complete. Comments added

--- 2022/day-03/README.md
+++ 2022/day-03/README.md
@@ -15,6 +15,7 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
+
 The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that appears in both compartments is lowercase p.
 The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that appears in both compartments is uppercase L.
 The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
--- 2022/day-03/inventoryControl.js
+++ 2022/day-03/inventoryControl.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 
 const getErrors = () => {
-
-
     fs.readFile('input.txt', 'utf8', (err, data) => {
         if(err) {
             console.log(err)
         }
     
         // Returns new array of found duplicates
-        const findDuplicates = (arr) =>{
+        const findDuplicates = (arr) => {
             const firstArray = arr[0];
             const duplicateCharacters =  [
                 ...new Set(
@@ -18,33 +16,60 @@ const getErrors = () => {
                     )
                 )
             ][0];
-            return duplicateCharacters
+            return duplicateCharacters;
         } 
 
         // Returns the priority Value of each found duplicate
-        const getPriorityValues = (priority) => {
+        const getPriorityValues = priority => {
             const upperCaseLimit = 65;
             const lowerCaseLimit = 96;
 
             return priority.charCodeAt(0) <= 90 ?
-                priority.charCodeAt(0) - upperCaseLimit + 27  : priority.charCodeAt(0) - lowerCaseLimit
+                priority.charCodeAt(0) - upperCaseLimit + 27 : priority.charCodeAt(0) - lowerCaseLimit
         }
 
-        const splitData = 
+        // Get front and back half of each rucksack
+        const getFrontAndBackHalf = ruckSackItems => {
+            const half = ruckSackItems.length / 2;
+            const frontHalf = ruckSackItems.slice(0, half);
+            const backHalf = ruckSackItems.slice(half);
+            const newArray = [ frontHalf, backHalf ];
+            return newArray;
+        }
+
+        // Divide data into groups of three
+        const divideByGroupsOfThree = group => {
+            const elfInGroup = 3;
+            let res = [];
+            for(let i = 0; i < group.length; i += elfInGroup) {
+                res.push(group.slice(i, i + elfInGroup));
+            }
+            return res;
+        }
+
+        // Part One
+        const findPrioritiesAndReturnSum = 
             data
                 .split('\n')
                 .map(inventory => inventory.split(''))
-                .map(item => {
-                    const half = item.length / 2;
-                    const frontHalf = item.slice(0, half);
-                    const backHalf = item.slice(half);
-                    return [frontHalf, backHalf];
-                })
+                .map(getFrontAndBackHalf)
                 .map(findDuplicates)
                 .map(getPriorityValues)
                 .reduce((a, b) => a + b)
-            
-        console.log(splitData)
+        
+        // Part Two
+        const findPrioritiesOfGroupsOfThree = 
+            divideByGroupsOfThree( data.split('\n').map(item => item.split('')))
+                .map(findDuplicates)
+                .map(getPriorityValues)
+                .reduce((a, b) => a + b)
+
+
+        console.log(findPrioritiesAndReturnSum, 'Is the priority total.')
+
+        console.log(findPrioritiesOfGroupsOfThree, 'Is the priority total for groups of three')
+
+
     })
 }
 
