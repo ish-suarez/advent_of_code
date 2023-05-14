881a65c7675e22e794541cbd164cc87481e8287e
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-05-13 23:52:47 -0500

    day 3 complete

--- 2015/day03/spherical_house.py
+++ 2015/day03/spherical_house.py
@@ -1,18 +1,43 @@
-x, y = 0, 0
-visited = 1
 startingAt = (0, 0)
-coordinates = [startingAt]
-receivedOneGift = []
+santaCoordinates = [startingAt]
+robotCoordinates = [startingAt]
 
-for step in open('data.txt').readline():
-    if step == '^':
-        y += 1
-    if step == 'v':
-        y -= 1
-    if step == '>':
-        x += 1
-    if step == '<':
-        x -= 1
-    coordinates.append((x, y))
+def filterOutDuplicatesAndFindSum(listOfCoordinates):
+    return len(set(listOfCoordinates))
+
+def filterOutDuplicatesFromBothCoordinates(list1, list2):
+    return len(set(list1 + list2))
+
+
+x, y = 0,0
+for index, step in enumerate(open('data.txt').readline()):
     
-print(len(set(coordinates)))
+    if index % 2 == 0:
+        if step == '^':
+            y += 1
+        if step == 'v':
+            y -= 1
+        if step == '>':
+            x += 1
+        if step == '<':
+            x -= 1
+        santaCoordinates.append((x, y))
+
+x, y = 0, 0
+for index, step in enumerate(open('data.txt').readline()):
+    if index % 2 != 0:
+        if step == '^':
+            y += 1
+        if step == 'v':
+            y -= 1
+        if step == '>':
+            x += 1
+        if step == '<':
+            x -= 1
+        robotCoordinates.append((x, y))
+
+santa, robot = filterOutDuplicatesAndFindSum(santaCoordinates), filterOutDuplicatesAndFindSum(robotCoordinates)
+
+print(f"Total number of houses that received at least one gift from santa: {santa}")
+print(f"Total number of houses that received at least one gift form robot santa: {robot}")
+print(f"Total houses that received at least one present: {filterOutDuplicatesFromBothCoordinates(santaCoordinates, robotCoordinates)}")
